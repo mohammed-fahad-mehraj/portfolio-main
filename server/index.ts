48eee41d9965ce913b54df1e7b022a39ceb057a8
Mohammed Fahad Mehraj <your_email@example.com>
2026-02-20 03:23:25 -0600

    Fix Railway start

--- server/index.ts
+++ server/index.ts
@@ -64,7 +64,7 @@ Message: ${message}
   }
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on ${port}`);
